45bb43004df6f17b9d9c6c97d50b7981e5197ac6
Amruth Shyju <amruthshyju@gmail.com>
2024-05-26 10:29:34 +0530

    change bycript from bycriptjs

--- controllers/admin-controllers.js
+++ controllers/admin-controllers.js
@@ -1,5 +1,5 @@
 import Admin from "../models/Admin.js"
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 
 export const addAdmin = async (req, res) => {
--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,5 +1,5 @@
 import User from "../models/User.js"
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 
 export const getAllUser=async(req,res)=>{
     let users;
